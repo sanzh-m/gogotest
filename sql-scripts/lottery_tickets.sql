e0c3c0b011cbb3dbe493e9ea360f20139eef4d4a
Sanzhar <sanzh98@gmail.com>
2021-04-17 21:17:42 +0800

    fix to sql script

--- sql-scripts/lottery_tickets.sql
+++ sql-scripts/lottery_tickets.sql
@@ -2,6 +2,7 @@ create table lottery_tickets (
                                  id uuid primary key,
                                  result boolean,
                                  draw_id integer references lottery_draws(id) on delete cascade,
+                                 client_id uuid references clients(id) on delete cascade,
                                  created_at numeric not null,
                                  last_modified_at numeric not null
 );
